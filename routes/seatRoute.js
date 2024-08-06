d0c94e97072b4e740c2d28432f03775357661cb7
nobislinkdev <101082815+nobislinkdev@users.noreply.github.com>
2024-08-06 17:27:53 +0530

    sds

--- routes/seatRoute.js
+++ routes/seatRoute.js
@@ -1,16 +1,19 @@
-const express = require('express');
-const { allocateSeats,allseats,updateseat,deleteseat } = require('../controller/seatController');
-const { getsearchAll } = require('../controller/searchController');
+const express = require("express");
+const {
+  allocateSeats,
+  allseats,
+  updateseat,
+  deleteseat,
+} = require("../controller/seatController");
+const { getsearchAll } = require("../controller/searchController");
 // const {  } = require('../controllers/');
-
 
 const router = express.Router();
 
-router.post('/create', allocateSeats);
-router.get('/read', allseats);
-router.post('/update/:id', updateseat);
-router.post('/delete/:id', deleteseat);
-router.get('/search', getsearchAll);
-
+router.post("/create", allocateSeats);
+router.get("/read", allseats);
+router.put("/update/:id", updateseat);
+router.post("/delete/:id", deleteseat);
+router.get("/search", getsearchAll);
 
 module.exports = router;
