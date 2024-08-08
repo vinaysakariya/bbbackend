cc256d077cbb5a9596f8d572306c08d60b11a549
nobislinkdev <101082815+nobislinkdev@users.noreply.github.com>
2024-08-08 11:22:16 +0530

    update

--- controller/bustInfoController.js
+++ controller/bustInfoController.js
@@ -1,29 +1,34 @@
 const Businfo = require("../models/busInfo");
 
-async function busDetails(req,res){
-    try{
-        const {date,location,busNumber,price}=req.body
+async function busDetails(req, res) {
+  try {
+    const { date, location, busNumber, price } = req.body;
 
-        const busdetails= await Businfo.create({
-            date,location,busNumber,price
-        })
-        res.satus(200).json({data:busdetails})
-    }catch(error){
-        res.satus(500).json(`error while fetching details ${error}`)
-    }
+    const busdetails = await Businfo.create({
+      date,
+      location,
+      busNumber,
+      price,
+    });
+    res.status(200).json({ data: busdetails });
+  } catch (error) {
+    res.status(500).json(`error while fetching details ${error}`);
+  }
 }
-async function busDetailsupdate(req,res){
-    try{
-        const {date,location,busNumber,price}=req.body
+async function busDetailsupdate(req, res) {
+  try {
+    const { date, location, busNumber, price } = req.body;
 
-        const busdetails= await Businfo.findByIdAndUpdate(req.params.id,{
-            date,location,busNumber,price
-        })
-        res.satus(200).json({data:busdetails})
-    }catch(error){
-        res.satus(500).json(`error while fetching details ${error}`)
-    }
+    const busdetails = await Businfo.findByIdAndUpdate(req.params.id, {
+      date,
+      location,
+      busNumber,
+      price,
+    });
+    res.status(200).json({ data: busdetails });
+  } catch (error) {
+    res.status(500).json(`error while fetching details ${error}`);
+  }
 }
 
-
-module.exports={busDetails,busDetailsupdate}
+module.exports = { busDetails, busDetailsupdate };
